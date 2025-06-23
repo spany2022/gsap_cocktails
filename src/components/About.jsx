e6ccfd27ea46bb20b08269809e798ebf3221633b
Sandesh <sandesh@lumosys.in>
2025-06-23 23:39:08 +0530

    Implement About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,32 +4,37 @@ import gsap from "gsap";
 import SplitText from "gsap/SplitText";
 
 const About = () => {
-    useGSAP(() => {
-        const titleSplit = SplitText.create('#about h2', {
-            type: 'words'
-        })
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#about h2", {
+      type: "words",
+    });
 
-        const scrollTimeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: '#about',
-                start: 'top center'
-            }
-        })
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
 
-        scrollTimeline.from(titleSplit.words, {
-            opacity:0,
-            duration:1,
-            yPercent: 100,
-            ease: 'expo.out',
-            stagger: 0.02
-        })
-        .from('.top-grid div, .bottom-grid div', {
-            opacity: 0,
-            duration: 1,
-            ease: 'power1.inOut',
-            stagger: 0.04,
-        }, '-=0.5')
-    })
+    scrollTimeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        duration: 1,
+        yPercent: 100,
+        ease: "expo.out",
+        stagger: 0.02,
+      })
+      .from(
+        ".top-grid div, .bottom-grid div",
+        {
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inOut",
+          stagger: 0.04,
+        },
+        "-=0.5"
+      );
+  });
   return (
     <div id="about">
       <div className="mb-16 md:px-0 px-5">
@@ -83,7 +88,7 @@ const About = () => {
           <img src="/images/abt3.png" alt="grid-img-3" />
         </div>
 
-                <div className="md:col-span-4">
+        <div className="md:col-span-4">
           <div className="noisy" />
           <img src="/images/abt4.png" alt="grid-img-4" />
         </div>
